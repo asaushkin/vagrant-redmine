5b6a21080397a1e0c807b9625b54c6958dd32332
Andrew G. Saushkin <asaushkin@gmail.com>
2016-04-04 12:38:02 +0300

    Changed to PostgreSQL 9.5 and Redmine 3.2.1. Fix install issues.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # Script to create a vagrant image with redmine ready to use.
 
-export REDMINE_VERSION=3.0.0
+export REDMINE_VERSION=3.2.1
+export PG_VERSION=9.5
 
 # Set non-interactive instaler mode, update repos.
 export DEBIAN_FRONTEND=noninteractive
@@ -13,23 +14,24 @@ echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main"
 
 apt-get update
 apt-get purge -y ruby
-apt-get autoremove -y
-apt-get upgrade
+#apt-get autoremove -y
+#apt-get upgrade
 
-apt-get -y install postgresql-9.4
+apt-get -y install postgresql-${PG_VERSION}
 
 # Ruby (RVM) requirements
 apt-get -y install g++ libreadline6-dev zlib1g-dev libssl-dev   \
   libyaml-dev libsqlite3-dev sqlite3 autoconf libgdbm-dev       \
   libncurses5-dev automake libtool bison pkg-config libffi-dev  \
-  postgresql-server-dev-9.4 libxslt-dev libxml2-dev imagemagick  \
+  postgresql-server-dev-${PG_VERSION} libxslt-dev libxml2-dev imagemagick  \
   libmagickwand-dev
 
 su -c 'createuser redmine && createdb redmine -O redmine' postgres
 
-echo "local redmine redmine peer" >> /etc/postgresql/9.4/main/pg_hba.conf
+echo "local redmine redmine peer" >> /etc/postgresql/${PG_VERSION}/main/pg_hba.conf
 
 adduser --system --shell=/bin/bash --home=/opt/redmine redmine
+echo "redmine ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/redmine
 
 wget -O - http://www.redmine.org/releases/redmine-${REDMINE_VERSION}.tar.gz 2>/dev/null | tar -C /opt/ -xvzf -
 rm -fr /opt/redmine
